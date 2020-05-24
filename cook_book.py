fb04c6ee8d151e0d4c2d8b07199ff5deaae4044d
TimBokarev <tim@bokarev.ru>
2020-05-24 17:24:42 +0200

    финальная версия для проверки преподавателем

--- cook_book.py
+++ cook_book.py
@@ -9,13 +9,11 @@ def read_file(file_name):
     return file_lines
 
 
-k = read_file("recipes.txt")
-
-
 def cook_book_creation(recipes_list):
 
     dish = []
-    cook_work = {}
+    cook_book = {}
+    recipes_list.append('')
 
     for items in recipes_list:
 
@@ -27,24 +25,79 @@ def cook_book_creation(recipes_list):
             cutted_dish.pop(0)
 
             list_of_ingradients = []
-            dict = {}
             for string in cutted_dish:
+                dict = {}
                 string = string.split(" | ")
                 dict["ingredient_name"] = string[0]
                 dict["quantity"] = string[1]
                 dict["measure"] = string[2]
                 list_of_ingradients.append(dict)
 
-            print(list_of_ingradients)
-
-            cook_work[dish[0]] = list_of_ingradients
+            cook_book[dish[0]] = list_of_ingradients
             dish = []
 
-    print(cook_work)
-
-cook_book_creation(k)
+    return cook_book
 
 
+book = cook_book_creation(read_file("recipes.txt"))
+
+
+for key in book:
+    print(key, end=" - ")
+print()
+dishes_request = input("введите через запятую блюда из предлагаемого списка: ")
+choice = dishes_request.split(',')
+guests_number = int(input("введите кол-во гостей: "))
+print(choice)
+
+# choice = ["Омлет", "Фахитос", "Утка по-пекински"]
+# guests_number = 2
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    shop_list_addition = {}
+    for dish in book:
+
+        if dish in dishes:
+
+            for k in book.get(dish):
+
+                shop_items = {}
+                shop_items["measure"] = k.get("measure")
+                shop_items["quantity"] = guests_number * int(k.get("quantity"))
+                ingradient = k.get("ingredient_name")
+                shop_list_addition[ingradient] = shop_items
+
+            for item in shop_list_addition:
+
+                if item in shop_list:
+                    dict1 = shop_list_addition.get(item)
+                    dict2 = shop_list.get(item)
+                    qua1 = dict1.get("quantity")
+                    qua2 = dict2.get("quantity")
+                    mes = dict2.get("measure")
+                    quames = {}
+                    quames["measure"] = mes
+                    quames["quantity"] = int(qua1) + int(qua2)
+                    shop_list[item] = quames
+
+                else:
+                    shop_list[item] = shop_list_addition.get(item)
+
+    return shop_list
+
+
+print(get_shop_list_by_dishes(choice, guests_number))
+
+
+# {
+#   'Картофель': {'measure': 'кг', 'quantity': 2},
+#   'Молоко': {'measure': 'мл', 'quantity': 200},
+#   'Помидор': {'measure': 'шт', 'quantity': 4},
+#   'Сыр гауда': {'measure': 'г', 'quantity': 200},
+#   'Яйцо': {'measure': 'шт', 'quantity': 4},
+#   'Чеснок': {'measure': 'зубч', 'quantity': 6}
+# }
 
 
 # cook_book = {
